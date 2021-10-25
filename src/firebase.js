b0549e255fadc550376795e68cb4087495223105
Leotans <saputraleotan@gmail.com>
2021-10-25 13:15:52 +0700

    jojo gantenk

--- src/firebase.js
+++ src/firebase.js
@@ -1,15 +1,15 @@
 import firebase from "firebase";
-import '@firebase/firestore'
+import "@firebase/firestore";
 
-console.log("init firebase")
+console.log("init firebase");
 const firebaseConfig = {
-    apiKey: "AIzaSyBqXr9_sjIkzFle6kccbaxn6xvd2bgJkL0",
-    authDomain: "leo-hci-2.firebaseapp.com",
-    projectId: "leo-hci-2",
-    storageBucket: "leo-hci-2.appspot.com",
-    messagingSenderId: "611617703717",
-    appId: "1:611617703717:web:00e21e84fb7f3f4601bccc",
-    measurementId: "G-E1VXVQMCM5"
+  apiKey: "AIzaSyAZgnfUV7hAv1eJpBVry7T0o7xm9qyjxRo",
+  authDomain: "hci-leo.firebaseapp.com",
+  projectId: "hci-leo",
+  storageBucket: "hci-leo.appspot.com",
+  messagingSenderId: "279549318888",
+  appId: "1:279549318888:web:75a570fa6b470199c71b5d",
+  measurementId: "G-KTXN2K41SR",
 };
 let Fire = firebase.initializeApp(firebaseConfig);
 
