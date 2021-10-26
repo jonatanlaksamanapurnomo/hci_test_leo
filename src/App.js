7c3a6a228ae75434ea378092c4edff0561c5b071
jonatanlaksamanapurnomo <jonatanlaksamanapurnomo@gmail.com>
2021-10-26 11:13:11 +0700

    try debug path

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-import Home from "./pages/home/index";
-import Timer from "./pages/Timer";
+// import Home from "./pages/home/index";
+// import Timer from "./pages/Timer";
 import Register from "./pages/register";
-import EndScene from "./pages/endscene";
+// import EndScene from "./pages/endscene";
 import {
     BrowserRouter as Router,
     Switch,
@@ -12,9 +12,9 @@ function App() {
     return (
         <Router>
             <Switch>
-                <Route exact path="/home" component={Home}/>
-                <Route exact path="/timer" component={Timer}/>
-                <Route exact path="/thankyou" component={EndScene}/>
+                {/*<Route exact path="/home" component={Home}/>*/}
+                {/*<Route exact path="/timer" component={Timer}/>*/}
+                {/*<Route exact path="/thankyou" component={EndScene}/>*/}
                 <Route exact path="/" component={Register}/>
             </Switch>
         </Router>
